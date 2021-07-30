96e8b08d61eb600370368cd4c24580b79a102778
liangchencloud <liang18611230766@163.com>
2021-07-30 14:49:13 +0800

    配置更改

---
+++ simpleoa/src/main/java/net/northking/springboot/shiro/CustomerRealm.java
@@ -0,0 +1,25 @@
+package net.northking.springboot.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+/**
+ * 自定义realm
+ */
+public class CustomerRealm extends AuthorizingRealm {
+    // 授权
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        return null;
+    }
+
+    // 认证
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
+}
---
+++ simpleoa/src/main/java/net/northking/springboot/util/ShiroConstant.java
@@ -0,0 +1,8 @@
+package net.northking.springboot.util;
+
+public class ShiroConstant {
+    /** 随机盐的位数 **/
+    public static final int SALT_LENGTH = 8;
+    /** hash的散列次数 **/
+    public static final int HASH_ITERATORS = 1024;
+}
